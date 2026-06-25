b9bb8dc5105455975ad6f64d723a5fbf6ad5bc64
daniM-T <danielmeye09@gmail.com>
2026-06-25 21:11:17 +0200

    Pre loader toegevoegd en nav blavk bar weggehaalt

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ function scrollNaarProjecten() {
     sectie.scrollIntoView({ behavior: "smooth" });
 }
 
+// PRELOADER
+window.addEventListener("load", function() {
+    const preloader = document.getElementById("preloader");
+
+    setTimeout(() => {
+        preloader.classList.add("fade-out");
+    }, 2300);
+});
+
+// NAVBAR SCROLL EFFECT
+window.addEventListener("scroll", function() {
+    const navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
